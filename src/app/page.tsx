b09e3471be03ca4c2729f2a46710c3072175ba77
suryansh <suryanshpatwal@gmail.com>
2025-02-05 18:07:35 +0530

    add MagicCardDemo and AnimatedTestimonialsDemo components; remove AVM; update next.config and package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+"use client";
 import { SpotlightNewDemo } from "@/components/HeroSection";
-import AVM from "@/components/AVM";
+import { MagicCardDemo } from "@/components/MagicCardDemo";
 import { MarqueeDemo } from "@/components/MarqueeDemo";
+import { AnimatedTestimonialsDemo } from "@/components/AnimatedTestimonialsDemo";
 
 export default function Home() {
   return (
     <div>
       <SpotlightNewDemo />
-      <AVM />
+      <MagicCardDemo />
       <MarqueeDemo />
+      <AnimatedTestimonialsDemo />
     </div>
   );
 }
