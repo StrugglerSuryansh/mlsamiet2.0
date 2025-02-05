f6dee277121c69690b1588045fcb178a02f694de
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-02-05 18:27:19 +0530

    fixed conflict

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-
   /* config options here */
-images: {
-    domains: ["images.unsplash.com"],
-  },
-=======
   images: {
-    domains: ['images.unsplash.com',"plus.unsplash.com"], // Add this line
+    domains: ["images.unsplash.com", "plus.unsplash.com"], // Add this line
   },
   /* config options here */
-  eslint:{
-    ignoreDuringBuilds:true
-  }
->>>>>>> e047f6419dd7cac3aa47736c1172a3ba74e725c6
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,9 @@
 import { SpotlightNewDemo } from "@/components/HeroSection";
 import { MagicCardDemo } from "@/components/MagicCardDemo";
 import { MarqueeDemo } from "@/components/MarqueeDemo";
-<<<<<<< HEAD
-import { AnimatedTestimonialsDemo } from "@/components/AnimatedTestimonialsDemo";
-=======
+
 import { AnimatedTestimonialsDemo } from "@/components/AnimatedTestimonials";
 import ContactForm from "@/components/ContactForm";
->>>>>>> e047f6419dd7cac3aa47736c1172a3ba74e725c6
 
 export default function Home() {
   return (
@@ -16,10 +13,7 @@ export default function Home() {
       <MagicCardDemo />
       <MarqueeDemo />
       <AnimatedTestimonialsDemo />
-<<<<<<< HEAD
-=======
       <ContactForm />
->>>>>>> e047f6419dd7cac3aa47736c1172a3ba74e725c6
     </div>
   );
 }
