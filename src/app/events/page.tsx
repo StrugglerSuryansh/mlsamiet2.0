efd8b1233c3380b43cccbd0669eaaeae456bddc2
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-02-06 16:37:43 +0530

    added events page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { BentoGridDemo } from "@/components/BentoGridDemo";
+import { CarouselDemo } from "@/components/Crousel";
+
+export default function Event() {
+
+    return (
+        <div>
+           <BentoGridDemo />
+           <CarouselDemo /> 
+        </div>
+    );
+}
