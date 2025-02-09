2ffe1f9253aa619fd59eb6a0a7e97ba1dd48d1bb
suryansh <suryanshpatwal@gmail.com>
2025-02-09 22:36:08 +0530

    changed stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,9 +30,8 @@ const Navbar = () => {
   return (
     <>
       <nav
-        className={`fixed left-0 right-0 top-0 z-[51] w-full py-4 px-4 sm:px-6 lg:px-6 border-b-2 border-cyan-200 ${
-          showBackground ? "backdrop-blur-md" : "bg-white"
-        }`}
+        className={`fixed left-0 right-0 top-0 z-[51] w-full py-4 px-4 sm:px-6 lg:px-6 border-b-2 border-cyan-200 ${showBackground ? "backdrop-blur-md" : "bg-white"
+          }`}
       >
         <div className="flex items-center justify-between max-w-[1920px] mx-auto">
           <div className="flex items-center gap-4">
@@ -62,7 +61,7 @@ const Navbar = () => {
             <NavbarItem href="/projects">Projects</NavbarItem>
             <NavbarItem href="/gallery">Gallery</NavbarItem>
             <NavbarItem href="/members">Members</NavbarItem>
-            <NavbarItem href="/auth/login" type="button">
+            <NavbarItem href="/joinus" type="button">
               Join Us
             </NavbarItem>
           </div>
