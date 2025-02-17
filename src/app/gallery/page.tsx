4d115b245a43a27eb7be17d9b63ef8c6a69c5f9a
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-02-18 00:49:33 +0530

    prod fix

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,44 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import heic2any from "heic2any";
+
 
 const gallery = () => {
-  const [convertedImages, setConvertedImages] = useState<{
-    [key: string]: string;
-  }>({});
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const heicImages = [
-        "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2276.HEIC",
-        "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2280.HEIC",
-        "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2283.HEIC",
-        "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2286.HEIC",
-        "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2288.HEIC",
-        // Add other HEIC image paths here if needed
-      ];
-
-      heicImages.forEach(async (src) => {
-        try {
-          const response = await fetch(src);
-          if (!response.ok) {
-            throw new Error(`Failed to fetch image: ${src}`);
-          }
-          const blob = await response.blob();
-          const convertedBlob = await heic2any({ blob, toType: "image/jpeg" });
-          const blobToUse = Array.isArray(convertedBlob)
-            ? convertedBlob[0]
-            : convertedBlob;
-          const convertedUrl = URL.createObjectURL(blobToUse);
-          setConvertedImages((prev) => ({ ...prev, [src]: convertedUrl }));
-          console.log(`Converted ${src} to ${convertedUrl}`);
-        } catch (error) {
-          console.error("Error converting HEIC image:", error);
-        }
-      });
-    }
-  }, []);
 
   return (
     <div id="media_section" className="mx-8 lg:pt-20 pt-2 relative">
@@ -54,63 +18,43 @@ const gallery = () => {
               className="h-auto max-w-full rounded-[10px] object-cover"
               width={500}
               height={500}
-              src={
-                convertedImages[
-                  "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2276.HEIC"
-                ] || "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2276.HEIC"
-              }
+              src={"/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/PXL_20241014_102457828.PORTRAIT.jpg"}
               alt="AVASAR Event"
             />
           </div>
           <div>
-            <img
+          <img
               className="h-auto max-w-full rounded-[10px] object-cover"
               width={500}
               height={500}
-              src={
-                convertedImages[
-                  "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2280.HEIC"
-                ] || "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2280.HEIC"
-              }
+              src={"/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/PXL_20241014_102457828.PORTRAIT.jpg"}
               alt="AVASAR Event"
             />
           </div>
           <div>
-            <img
+          <img
               className="h-auto max-w-full rounded-[10px] object-cover"
               width={500}
               height={500}
-              src={
-                convertedImages[
-                  "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2283.HEIC"
-                ] || "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2283.HEIC"
-              }
+              src={"/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/PXL_20241014_102457828.PORTRAIT.jpg"}
               alt="AVASAR Event"
             />
           </div>
           <div>
-            <img
+          <img
               className="h-auto max-w-full rounded-[10px] object-cover"
               width={500}
               height={500}
-              src={
-                convertedImages[
-                  "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2286.HEIC"
-                ] || "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2286.HEIC"
-              }
+              src={"/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/PXL_20241014_102457828.PORTRAIT.jpg"}
               alt="AVASAR Event"
             />
           </div>
           <div>
-            <img
+          <img
               className="h-auto max-w-full rounded-[10px] object-cover"
               width={500}
               height={500}
-              src={
-                convertedImages[
-                  "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2288.HEIC"
-                ] || "/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/IMG_2288.HEIC"
-              }
+              src={"/MLSA_WEBSITE/EVENT_PHOTO/AVSAR/PXL_20241014_102457828.PORTRAIT.jpg"}
               alt="AVASAR Event"
             />
           </div>
