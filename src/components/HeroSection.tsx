800bb6377a8a4c40590abee067984a25155d1d2e
suryansh <suryanshpatwal@gmail.com>
2025-02-24 18:30:20 +0530

    fixed spacing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Spotlight } from "@/components/ui/spotlight-new";
 
 export function SpotlightNewDemo() {
   return (
-    <div className="h-[40rem] w-full rounded-md flex items-center justify-center bg-white/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[42rem] w-full rounded-md flex items-center justify-center bg-white/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden ">
       <Spotlight />
       <div className="p-4 max-w-7xl mx-auto relative w-full flex flex-col items-center justify-center z-10">
         <h1 className="text-4xl md:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-cyan-50 to-cyan-400 bg-opacity-50">
