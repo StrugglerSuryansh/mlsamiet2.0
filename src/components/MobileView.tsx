5885df86d27183151cd231725757c82378f0b4c2
suryansh <suryanshpatwal@gmail.com>
2025-02-25 21:39:05 +0530

    fixed stuff

--- src/components/MobileView.tsx
+++ src/components/MobileView.tsx
@@ -13,6 +13,13 @@ const MobileView = ({ visible }: MobileMenuProps) => {
     <div className="fixed inset-x-0 top-[72px] z-50 md:hidden">
       <div className="bg-gradient-to-b from-[#042179] to-[#949fc0] px-4 py-4 shadow-lg">
         <div className="mx-auto max-w-[1920px]">
+
+          <NavbarItem href="https://mlsamiet.vercel.app/" mobileView={true}>
+            Home
+          </NavbarItem>
+          <NavbarItem href="/knowmore" mobileView={true}>
+            About Us
+          </NavbarItem>
           <NavbarItem href="/events" mobileView={true}>
             Events
           </NavbarItem>
